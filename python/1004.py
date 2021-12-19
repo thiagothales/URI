184c5b720378b04ce17c7979e52f52d32e351ae1
thiago <tthiagothales@gmail.com>
2021-12-19 00:04:24 -0300

    Beginner

---
+++ python/1004.py
@@ -0,0 +1,4 @@
+p1 = int(input())
+p2 = int(input())
+prod = (p1 * p2)
+print("PROD = {}".format(prod))
---
+++ python/1007.py
@@ -0,0 +1,3 @@
+A, B, C, D = map(int, input().split(" "))
+diferenca = (A * B - C * D)
+print('DIFERENÇA = {}'.format(diferenca))
